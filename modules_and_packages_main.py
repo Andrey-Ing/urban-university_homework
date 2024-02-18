d67b52b6e03ec5ace76a65eb5c8fa2fc0501e10e
i <andrey.vt@mail.ru>
2024-02-18 23:29:53 +1200

    Сделано

---
+++ modules_and_packages_main.py
@@ -0,0 +1,5 @@
+import modules_and_packages_module as module
+
+
+module.hello_1()
+module.hello_2()
---
+++ modules_and_packages_module.py
@@ -0,0 +1,6 @@
+def hello_1():
+    print('Hello World!\tfrom hello_1')
+
+
+def hello_2():
+    print('Hello World!\tfrom hello_2')
