4256aa0023d8bc757d491a74d0070c904f265633
i <andrey.vt@mail.ru>
2024-03-09 22:16:27 +1200

    В работе

--- classes and objects practice.py
+++ classes and objects practice.py
@@ -1,18 +1,21 @@
 from random import *
 
+from termcolor import *
+
+
 class Man:
     def __init__(self, name):
         self.name = name
         self.fullness = 20
-        self.food = 10
-        self.money = 0
+        self.food = 20
+        self.money = 50
 
     def __str__(self):
         return 'я - {}, сытость {}, еды осталось {}, денег осталось {}'.format(
             self.name, self.fullness, self.food, self.money)
 
     def eat(self):
-        if self.food > 10:
+        if self.food >= 10:
             print('{} поел'.format(self.name))
             self.fullness += 10
             self.food -= 10
@@ -22,14 +25,22 @@ class Man:
     def work(self):
         print('{} ходил на работу'.format(self.name))
         self.money += 50
-        self.fullness -= 10
+        self.fullness -= 20
 
     def play_DOTA(self):
         print('{} играл в доту целый день'.format(self.name))
         self.fullness -= 10
 
+    def shopping(self):
+        if self.money >= 50:
+            print('{} сходил в магазин за едой'.format(self.name))
+            self.money -= 50
+            self.food += 50
+        else:
+            print('{} деньги кончились'.format(self.name))
+
     def act(self):
-        if self.fullness <= 0:
+        if self.fullness < 0:
             print('{} умер...'.format(self.name))
             return
         dice = randint(1, 6)
@@ -49,8 +60,6 @@ class Man:
 
 vasya = Man(name='Вася')
 for day in range(1, 21):
-    cprint('============== день {} =============='.format(day), color='yellow')
+    cprint('============== день {} =============='.format(day), color='green')
     vasya.act()
     print(vasya)
-
-
