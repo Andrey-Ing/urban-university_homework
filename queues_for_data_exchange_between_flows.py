2b6b2778736176f788dccfc584044a0a4a37adf7
User Namovich <username@yandex.ru>
2024-05-02 12:08:44 +1200

    В работе

---
+++ queues_for_data_exchange_between_flows.py
@@ -0,0 +1,28 @@
+from threading import Thread, Lock
+from time import sleep
+from queue import Queue
+
+
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        is_busy = False
+
+
+    number(int) - номер
+    стола, is_busy(bool)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
