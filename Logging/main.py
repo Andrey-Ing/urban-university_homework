2b0267b3d1a702b07f33b6baee8ae05e25e3054b
i <andrey.vt@mail.ru>
2024-05-29 23:04:07 +1200

    в работе

--- Logging/main.py
+++ Logging/main.py
@@ -9,16 +9,18 @@ sites = ['https://www.youtube.com/', 'https://instagram.com', 'https://wikipedia
 
 for site in sites:
     # ДОПОЛНИТЬ КОД ЗДЕСЬ
+    code = None
     try:
-        logging.basicConfig(level=logging.INFO, filename="success_responses.log", filemode="w")
+        logging.basicConfig(level=logging.INFO, filename="success_responses.log", filemode="w", force=True)
         response = rq.get(site, timeout=3)
+        code = response.status_code
         print(response)
-        logging.info(f'\'{site}\', response - {response.status_code}')
+        logging.info(f'\'{site}\', response - {code}')
 
     except rq.exceptions.RequestsWarning:
-        logging.basicConfig(level=logging.WARNING, filename="bad_responses.log", filemode="w")
-        logging.warning(f'\'{site}\', response - {response.status_code}')
+        logging.basicConfig(level=logging.WARNING, filename="bad_responses.log", filemode="w", force=True)
+        logging.warning(f'\'{site}\', response - {code}')
 
     except (rq.exceptions.ConnectionError, rq.exceptions.Timeout):
-        logging.basicConfig(level=logging.ERROR, filename="blocked_responses.log", filemode="w")
+        logging.basicConfig(level=logging.ERROR, filename="blocked_responses.log", filemode="w", force=True)
         logging.error(f'\'{site}\', NO CONNECTION')
