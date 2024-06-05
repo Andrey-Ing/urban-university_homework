74607f8c7fb43fe7db152658f24ecb14a87628b3
Andrey-Ing <andrey.vt@mail.ru>
2024-06-05 18:04:52 +1200

    готово

--- date_and_time.py
+++ date_and_time.py
@@ -2,15 +2,27 @@ from datetime import datetime
 
 
 class SuperDate(datetime):
+    def __init__(self, hemispheres, year, month, day, hour=0, minute=0, second=0,
+                 microsecond=0, tzinfo=None, *args, fold=0):
+
+        self._times_of_day = {(0, 1, 2, 3, 4, 5): 'Night',
+                              (6, 7, 8, 9, 10, 11): 'Morning',
+                              (12, 13, 14, 15, 16, 17): 'Day',
+                              (18, 19, 20, 21, 22, 23): 'Evening'}
+        self._season = None
+        self.current_hemispheres = None
+        self.set_hemispheres(hemispheres)
+
     def __new__(cls, hemispheres, year, month, day, hour=0, minute=0, second=0,
                 microsecond=0, tzinfo=None, *args, fold=0):
         instance = super().__new__(cls, year=year, month=month, day=day, hour=hour, minute=minute,
                                    second=second, microsecond=microsecond, tzinfo=tzinfo, *args, fold=fold)
 
-        cls.season = cls._hemispheres_inite(cls, hemispheres)
         return instance
 
-    def _hemispheres_inite(self, hemispheres):
+    def set_hemispheres(self, hemispheres):
+
+        self.current_hemispheres = hemispheres
 
         northern_season = {(3, 4, 5): 'Spring', (6, 7, 8): 'Summer',
                            (9, 10, 11): 'Autumn', (12, 1, 2): 'Winter'}
@@ -19,23 +31,36 @@ class SuperDate(datetime):
                            (3, 4, 5): 'Autumn', (6, 7, 8): 'Winter'}
 
         if hemispheres == 'northern':
-            return northern_season
+            self._season = northern_season
         elif hemispheres == 'southern':
-            return southern_season
+            self._season = southern_season
         else:
             raise ValueError('Должно быть задано либо северное("northern"),'
                              'либо южное("southern") полушарие')
 
-    def set_hemispheres(self, hemispheres):
-        self.season = self._hemispheres_inite(hemispheres)
+    def get_hemispheres(self):
+        return self.current_hemispheres
 
     def get_season(self):
-        for ind_tuple in self.season:
+        for ind_tuple in self._season:
             if self.month in ind_tuple:
-                return self.season[ind_tuple]
+                return self._season[ind_tuple]
+
+    def get_time_of_day(self):
+        for ind_tuple in self._times_of_day:
+            if self.hour in ind_tuple:
+                return self._times_of_day[ind_tuple]
 
 
-rr = SuperDate('northern', 1999, 12, 4)
-print(rr.get_season())
-rr.set_hemispheres('southern')
-print(rr.get_season())
+sd = SuperDate('northern', 1999, 12, 4, 3, 45)
+
+print(sd.date(), sd.time())
+
+print(sd.get_hemispheres())
+print(sd.get_season())
+print('*' * 9)
+sd.set_hemispheres('southern')
+print(sd.get_hemispheres())
+print(sd.get_season())
+print('*' * 9)
+print(sd.get_time_of_day())
