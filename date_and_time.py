bf12c8739ddacf5ed8ccf436a3ed3bb0eb61e11c
Andrey-Ing <andrey.vt@mail.ru>
2024-06-05 17:14:23 +1200

    в работе

--- date_and_time.py
+++ date_and_time.py
@@ -1,113 +1,41 @@
 from datetime import datetime
 
-print(datetime)
-
 
 class SuperDate(datetime):
-
-    def __init__(self, hemispheres, year, month=None, day=None, hour=0, minute=0, second=0,
+    def __new__(cls, hemispheres, year, month, day, hour=0, minute=0, second=0,
                 microsecond=0, tzinfo=None, *args, fold=0):
-        super().__init__(year=year, month=month, day=day, hour=hour, minute=minute, second=second,
-                         microsecond=microsecond, tzinfo=tzinfo, *args, fold=fold)
+        instance = super().__new__(cls, year=year, month=month, day=day, hour=hour, minute=minute,
+                                   second=second, microsecond=microsecond, tzinfo=tzinfo, *args, fold=fold)
 
-        self.season = None
+        cls.season = cls._hemispheres_inite(cls, hemispheres)
+        return instance
+
+    def _hemispheres_inite(self, hemispheres):
+
+        northern_season = {(3, 4, 5): 'Spring', (6, 7, 8): 'Summer',
+                           (9, 10, 11): 'Autumn', (12, 1, 2): 'Winter'}
+
+        southern_season = {(9, 10, 11): 'Spring', (12, 1, 2): 'Summer',
+                           (3, 4, 5): 'Autumn', (6, 7, 8): 'Winter'}
+
         if hemispheres == 'northern':
-            self.season = {3:'Summer', 8:'Autumn', 'Winter', 'Spring'}
+            return northern_season
+        elif hemispheres == 'southern':
+            return southern_season
+        else:
+            raise ValueError('Должно быть задано либо северное("northern"),'
+                             'либо южное("southern") полушарие')
 
-
-            В
-            северном
-            полушарии
-            весна
-            начинается
-            1
-            марта, лето — 1
-            июня, осень — 1
-            сентября, а
-            зима — 1
-            декабря.В
-            южном
-            полушарии
-            весна
-            начинается
-            1
-            сентября, лето — 1
-            декабря, осень — 1
-            марта, зима — 1
-            июня.
-            3: «Лето», 8: «Осень», «Зима», «Весна».
-        = {3:'Summer', 8:'Autumn', 'Winter', 'Spring'}
-
-        southern
-        northern
+    def set_hemispheres(self, hemispheres):
+        self.season = self._hemispheres_inite(hemispheres)
 
     def get_season(self):
-        self._month
+        for ind_tuple in self.season:
+            if self.month in ind_tuple:
+                return self.season[ind_tuple]
 
 
-
-
-
-        1.
-        get_season - должен
-        возвращать
-        сезон
-        года(Summer, Autumn, Winter, Spring)
-        2.
-        get_time_of_day - должен
-        возвращать
-        время
-        суток
-        (Morning: 6-12; Day: 12-18, Evening: 18-0, Night: 0-6)(последнее
-        число
-        не
-        включено
-        в
-        промежуток)
-
-
-        Пример
-        работы
-        класса:
-
-        a = SuperDate(2024, 2, 22, 12)
-        print(a.get_season())
-        print(a.get_time_of_day())
-
-        Вывод
-        на
-        консоль:
-        Winter
-        Day
-
-
-
-def __init__(self, id, name, Add, Emails):
-    class Emp():
-        def __init__(self, id, name, Add):
-            self.id = id
-            self.name = name
-            self.Add = Add
-
-    # Class freelancer inherits EMP
-    class Freelance(Emp):
-        def __init__(self, id, name, Add, Emails):
-            super().__init__(id, name, Add)
-            self.Emails = Emails
-
-    Emp_1 = Freelance(103, "Suraj kr gupta", "Noida", "KKK@gmails")
-    print('The ID is:', Emp_1.id)
-    print('The Name is:', Emp_1.name)
-    print('The Address is:', Emp_1.Add)
-    print('The Emails is:', Emp_1.Emails)
-
-
-a = SuperDate(2024, 2, 22, 12)
-print(a.get_season())
-print(a.get_time_of_day())
-
-
-class Freelance(Emp):
-    def __init__(self, id, name, Add, Emails):
-        super().__init__(id, name, Add)
-        self.Emails = Emails
+rr = SuperDate('northern', 1999, 12, 4)
+print(rr.get_season())
+rr.set_hemispheres('southern')
+print(rr.get_season())
