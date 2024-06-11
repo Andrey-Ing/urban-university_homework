9fcb0b778b3e97195afb9334a2dd328328786c40
Andrey-Ing <andrey.vt@mail.ru>
2024-06-11 18:29:09 +1200

    в работе

--- CSV_files/main.py
+++ CSV_files/main.py
@@ -1,33 +1,3 @@
-def write_holiday_cities(first_letter):
-    # РЕАЛИЗАЦИЯ
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-import pprint
-
-###
-# Формат CSV == Comma-Separated Values
-# - строки делятся переносом строки
-# - ячейки делятся запятыми
-# - если ячейка содержит запятую или перенос строки, ячейка обрамяляется кавычками
-# - если внутри системных кавычек есть кавычки, они пишутся в виде двойных кавычек
-
-# Формат простой и поддерживается многими системами программами по работе с таблицами.
-
-
-###
-# Библиотека csv
-
 import csv
 import pprint
 
@@ -47,34 +17,39 @@ fieldnames = ['name', 'visited', 'wants_to_visit']
 
 inventory_of_stash = []
 
-with open('CSV_files/travel-notes.csv', 'r', newline='') as csv_file:
-    csv_data = csv.reader(csv_file)
-    for row in csv_data:
-        yy = row[1].split(sep=';')
-        print(yy)
-        for y in yy:
-            y.strip()
-        print(yy, ')')
-
-    print(yy[1])
+# Have visited:
+# Want to visit:
+# No one has been to:
+# In the end we will go to: Almaty
 
 
+with open('travel-notes.csv', 'r', newline='') as csv_file_input:
+    read_csv = csv.DictReader(csv_file_input, fieldnames=fieldnames)
+    have_visited_set = set()
+    want_to_visit_set = set()
 
-    #
-    #
-    #
-    #
-    # for row in csv_data:
-    #     inventory_of_stash.append(row)
-    #     pprint.pprint(row)
-    #
+    for data in read_csv:
+        if data['name'][0] == 'a'.upper():
+            visited_list = data['visited'].split(sep=';')
+            visited_list = [vis.strip() for vis in visited_list]  # на всякий случай удаляем пробелы
+            have_visited_set.update(visited_list)
 
-# print(f'Вся информация о таблице: {inventory_of_stash}')
-#
-# # csv.writer() - метод для создания (или дополнения) csv файла
-#
-# new_artifact_for_stash = ['Sacred Lingam', '1935', '1']
-#
-# with open('external_data/Indiana_stash.csv', 'w', newline='') as out_csv:
-#     writer = csv.writer(out_csv)  # <_csv.writer object at 0x03B0AD80>
-#     writer.writerow(new_artifact_for_stash)
+            wants_to_visit_list = data['wants_to_visit'].split(sep=';')
+            wants_to_visit_list = [vis.strip() for vis in wants_to_visit_list]  # на всякий случай удаляем пробелы
+            want_to_visit_set.update(wants_to_visit_list)
+
+    no_one_has_been_set = want_to_visit_set.difference(have_visited_set)
+
+    have_visited = list(have_visited_set)
+    want_to_visit = list(want_to_visit_set)
+    no_one_has_been = list(no_one_has_been_set)
+
+    have_visited.sort()
+    want_to_visit.sort()
+    no_one_has_been.sort()
+
+
+
+
+
+
