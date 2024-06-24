12b94fabf95b0a95211d962b80a792231030bb20
i <andrey.vt@mail.ru>
2024-06-24 21:46:44 +1200

    в работе

--- Object_recognition/main.py
+++ Object_recognition/main.py
@@ -4,18 +4,31 @@ cap_img = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap_img.read()
-
+    frame = cv2.resize(frame, (200, 200))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     eyes = cv2.CascadeClassifier('haarcascade_eye.xml')
-    result = eyes.detectMultiScale(cap_img, scaleFactor=1, minNeighbors=1)
+    result = eyes.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+
+    # face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    #
+    # face_img = face.detectMultiScale(gray, 1.1, 2)
+    #
+    #
+    # for (x, y, w, h) in face_img:
+    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
 
     print(result)
 
+    for (x, y, w, h) in result:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (45, 89, 255), thickness=-1)
+
+
 
     cv2.imshow('Blured eyes', frame)
     key_press = cv2.waitKey(30)
-    if key_press == ord('q'):
+    if key_press == 0x1B:  # Esc
         break
 
 cap_img.release()
