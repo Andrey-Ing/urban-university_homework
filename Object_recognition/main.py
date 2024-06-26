4ac78752976132b64d87438120009b9c276cfc3e
Andrey-Ing <andrey.vt@mail.ru>
2024-06-26 18:31:31 +1200

    в работе

--- Object_recognition/main.py
+++ Object_recognition/main.py
@@ -4,30 +4,43 @@ cap_img = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap_img.read()
-    frame = cv2.resize(frame, (200, 200))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     eyes = cv2.CascadeClassifier('haarcascade_eye.xml')
-    result = eyes.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+    result_eyes = eyes.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
 
-    # face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    #
-    # face_img = face.detectMultiScale(gray, 1.1, 2)
-    #
-    #
-    # for (x, y, w, h) in face_img:
-    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    pt1_area = (frame.shape[1], 0)
+    pt2_area = (0, 0)
+
+    print(pt1_area, pt2_area)
+
+    for (x, y, w, h) in result_eyes:
+        if x < pt1_area[0]:
+            pt1_area = x, y
+
+        if y + h > pt2_area[1]:
+            pt2_area = x + w, y + h
+
+
+    cv2.rectangle(frame, pt1_area, pt2_area, (0, 255, 0), 2)
+
+
+    #cv2.rectangle(frame, (400, 400), (400, 400), (0, 255, 0), 2)
+    # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    print(result_eyes)
 
 
 
-    print(result)
+    #print(result)
 
-    for (x, y, w, h) in result:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (45, 89, 255), thickness=-1)
+    for (x, y, w, h) in result_eyes:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (45, 89, 255), thickness=1)
 
 
 
     cv2.imshow('Blured eyes', frame)
-    key_press = cv2.waitKey(30)
+    key_press = cv2.waitKey(50)
     if key_press == 0x1B:  # Esc
         break
 
