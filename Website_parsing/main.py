3d570a885971e88ad73441ff39c736f20f66129c
Andrey-Ing <andrey.vt@mail.ru>
2024-07-03 20:59:49 +1200

    в работе

--- Website_parsing/main.py
+++ Website_parsing/main.py
@@ -1,5 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
+import re
 import lxml
 
 url = 'https://coinmarketcap.com'
@@ -24,14 +25,38 @@ soup = BeautifulSoup(src, 'lxml')
 #tr class="sc-240ce903-0 iPeVVh"
 #selector = "tr.iPeVVh.sc-240ce903-0span, p.ehyBa-d.sc-71024e3e-0"
 selector = "td"
-ii = soup.select(selector)
+#ii = soup.select(selector)
+
+#ii = soup.find_all('a.span', class_='circle')
+
+
+ii = soup.css.select('td > a > span.circle')
+
+#ii = soup.css.select('div > table > tbody > tr > td')
+
 #ii = soup.findAll('$')
 print(len(ii))
 # считываем заголовок страницы
 res_str = ''
 for i in ii:
-    print(i.text)
-    res_str += str(i)+'\n'
+    #print(i.parent.parent.parent.find(string=lambda text: isinstance(text, Comment)).parent)
+    #print(i.parent.parent.parent.find_all('tr', string=re.compile('/d')))
+    print(i.parent.parent.parent)
+    #print(i.text)
+    res_str += str(i) + '\n'
 with open('test', 'w', newline='', encoding='utf-8') as file:
     file.write(res_str)
-# Программа выведет: Курсы - Блог компании Селектел
+
+###
+# < td > < b > Address: < / b > < / td >
+# < td > My
+# home
+# address < / td >
+# ###
+#
+# address = soup.find(text="Address:")
+# b_tag = address.parent
+# td_tag = b_tag.parent
+# next_td_tag = td_tag.findNext('td')
+# print
+# next_td_tag.contents[0]
