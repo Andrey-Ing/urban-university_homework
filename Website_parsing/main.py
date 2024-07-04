9d4d714e2d92d79f77080f39ff614c1a6711d480
Andrey-Ing <andrey.vt@mail.ru>
2024-07-04 17:05:21 +1200

    готово

--- Website_parsing/main.py
+++ Website_parsing/main.py
@@ -1,86 +1,79 @@
 import csv
-
 import requests
-from bs4 import BeautifulSoup, Comment
-import re
+from bs4 import BeautifulSoup
 import lxml
-
-url = 'https://coinmarketcap.com'
-
-# отправляем запрос с заголовками по нужному адресу
-req = requests.get(url)
-if req.status_code != 200:
-    raise ConnectionError(f'Ответ от сайта {url} не 200 "OK"')
-
-src = req.text
-
-first_part_len = 10
-second_part_len = 90
-
-first_part_name = ['tt']
-first_part_money = []
-second_part_name = []
-second_part_money = []
-
-soup = BeautifulSoup(src, 'lxml')
-
-first_part_name_select = soup.css.select('div > div > p.ehyBa-d')
-first_part_money_select = soup.css.select('td > div.gPTgRa')
-
-second_part_name_select = soup.css.select('td > a > span.circle')
-
-for name in first_part_name_select:
-    first_part_name.append(name.text)
-
-for money in first_part_money_select:
-    first_part_money.append(float(money.text.lstrip('$').replace(',', '')))
-
-for name in second_part_name_select:
-    second_part_name.append(name.next_element.text)
-    second_part_money.append(name.parent.parent.parent.find(string=re.compile(r'\d')))
-
-if (len(first_part_name) != len(first_part_money) != first_part_len) or \
-        (len(second_part_name) != len(second_part_money) != second_part_len):
-    raise ValueError(f'Ошибка в парсинге данных')
+import re
+import time
 
 
-print(len(first_part_name))
+def write_cmc_top():
+    url = 'https://coinmarketcap.com'
+    # отправляем запрос с заголовками по нужному адресу
+    req = requests.get(url)
+    if req.status_code != 200:
+        raise ConnectionError(f'Ответ от сайта {url} не 200 "OK"')
+
+    src = req.text
+
+    first_part_name = []
+    first_part_money = []
+    second_part_name = []
+    second_part_money = []
+
+    # для проверки данных после парсинга
+    first_part_len = 10
+    second_part_len = 90
+
+    soup = BeautifulSoup(src, 'lxml')
+    # На сайте первые 10 значений имени и цены берутся по первым селекторам
+    first_part_name_select = soup.css.select('div > div > p.ehyBa-d')
+    first_part_money_select = soup.css.select('td > div.gPTgRa')
+    # Остальные 90 значений имени и цены берутся по второму селектору имени из которого потом получим и цену
+    second_part_name_select = soup.css.select('td > a > span.circle')
+
+    for name in first_part_name_select:
+        first_part_name.append(name.text)
+
+    for money in first_part_money_select:
+        first_part_money.append(float(money.text.lstrip('$').replace(',', '')))
+
+    for name in second_part_name_select:
+        second_part_name.append(name.next_element.text)
+        second_part_money.append(float(name.parent.parent.parent.find(string=re.compile(r'\d'))))
+
+    # проверка данных
+    if (len(first_part_name) == len(first_part_money) == first_part_len) and \
+            (len(second_part_name) == len(second_part_money) == second_part_len):
+        pass
+    else:
+        raise ValueError(f'Ошибка в парсинге данных')
+
+    total_market_capitalization = 0.0
+
+    for value in first_part_money:
+        total_market_capitalization += value
+
+    for value in second_part_money:
+        total_market_capitalization += value
+
+    # время - для названия файла
+    current_time = time.time()
+    local_time = time.localtime(current_time)
+    formatted_time = time.strftime("%H.%M %d.%m.%Y", local_time)
+
+    with open(f'{formatted_time}.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Name', 'MC', 'MP']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=' ')
+
+        writer.writeheader()
+
+        for i in range(len(first_part_name)):
+            writer.writerow({'Name': first_part_name[i], 'MC': first_part_money[i],
+                             'MP': round(first_part_money[i] / total_market_capitalization * 100, 1)})
+
+        for i in range(len(second_part_name)):
+            writer.writerow({'Name': second_part_name[i], 'MC': second_part_money[i],
+                             'MP': round(second_part_money[i] / total_market_capitalization * 100, 1)})
 
 
-with open('names.csv', 'w', newline='') as csvfile:
-    fieldnames = ['first_name', 'last_name']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-
-# #ii = soup.css.select('div > table > tbody > tr > td')
-#
-# #ii = soup.findAll('$')
-# print(len(ii))
-# # считываем заголовок страницы
-# res_str = ''
-# for i in ii:
-#     #print(i.parent.parent.parent.find(string=lambda text: isinstance(text, Comment)).parent)
-#     print(i.parent.parent.parent.find(string=re.compile(r'\d')))
-#     #print(i.parent.parent.parent)
-#     #print(i.text)
-#     res_str += str(i) + '\n'
-# with open('test', 'w', newline='', encoding='utf-8') as file:
-#     file.write(res_str)
-#
-# ###
-# # < td > < b > Address: < / b > < / td >
-# # < td > My
-# # home
-# # address < / td >
-# # ###
-# #
-# # address = soup.find(text="Address:")
-# # b_tag = address.parent
-# # td_tag = b_tag.parent
-# # next_td_tag = td_tag.findNext('td')
-# # print
-# # next_td_tag.contents[0]
+write_cmc_top()
