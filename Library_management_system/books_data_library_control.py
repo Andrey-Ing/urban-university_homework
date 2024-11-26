c59eae08a15bf5c62a4ce587b4a3326c34043dba
Andrey-Ing <andrey.vt@mail.ru>
2024-11-26 15:20:31 +1200

    В работе

--- Library_management_system/books_data_library_control.py
+++ Library_management_system/books_data_library_control.py
@@ -1,11 +1,29 @@
+"""
+Этот модуль представляет собой базу данных и обеспечивает систему управления данными
+библиотеки книг. Данные представляет собой JSON файл в котором записаны ID, название,
+автор книги, а также её наличие в библиотеке. ID генерируется случайным образом
+и уникально для каждой книги. Поддерживается добавления, удаления, поиска и изменение
+ статуса книг в библиотеке.
+"""
+
 import re
 from random import randint
 import json
 
-storage_books_name = 'books_data_library.json'
+storage_books_name = 'books_data_library.json'  # файл уже должен существовать
 
 
 def get_new_id(books_dict: dict) -> str:
+    """
+    Генерирует новый уникальный идентификатор для книги состоящий из двух букв, дефиса и
+    числа, которое может иметь произвольную длину.
+
+    Аргументы:
+    books_dict (dict): Словарь, содержащий существующие книги и их данные.
+
+    Возвращает:
+    str: Новый уникальный идентификатор для книги.
+    """
     prefix_chr = chr(randint(ord('A'), ord('Z'))) + chr(randint(ord('A'), ord('Z')))
     bias_mul_id = 10
     attempts = 10
@@ -19,15 +37,19 @@ def get_new_id(books_dict: dict) -> str:
 
 
 class BookBase:
+    """
+    Базовое представление книги с возможностью выдачи последовательных данных.
+    """
+
     def __init__(self, title: str, author: str, year: int, status: bool = True):
         self.title = title
         self.author = author
         self.year = year
-        self.status = status
+        self.status = status  # True - на месте в библиотеке, False - выдана
 
     def get_serial_data(self):
         return [self.title, self.author, self.year, self.status]
-
+    # Для возможности доступа к последовательным данным по индексу
     title_idx = 0
     author_idx = 1
     year_idx = 2
@@ -35,17 +57,42 @@ class BookBase:
 
 
 def _library_read_data(filename: str) -> dict:
+    """
+    Считывает данные из файла JSON.
+
+    Аргументы:
+    filename (str): Имя файла для чтения.
+
+    Возвращает:
+    dict: Данные, считанные из файла.
+    """
     with open(filename, 'r') as stor_r:
         data = json.load(stor_r)
         return data
 
 
 def _library_save_data(filename: str, data: dict):
+    """
+    Сохраняет данные в файл JSON.
+
+    Аргументы:
+    filename (str): Имя файла для сохранения,
+    data (dict): Данные для сохранения.
+    """
     with open(filename, 'w') as stor_w:
         json.dump(data, stor_w, indent=4)
 
 
 def add_book_in_library(book: BookBase) -> str:
+    """
+    Добавляет книгу в библиотеку.
+
+    Аргументы:
+    book (BookBase): Книга для добавления.
+
+    Возвращает:
+    str: Уникальны идентификатор добавленной книги.
+    """
     data_books = _library_read_data(storage_books_name)
     new_id = get_new_id(data_books)
     data_books[new_id] = book.get_serial_data()
@@ -54,6 +101,15 @@ def add_book_in_library(book: BookBase) -> str:
 
 
 def delete_book_in_library(book_id: str) -> bool:
+    """
+    Удаляет книгу из библиотеки.
+
+    Аргументы:
+    book_id (str): Идентификатор книги для удаления.
+
+    Возвращает:
+    bool: True, если книга была удалена, False в противном случае.
+    """
     data_books = _library_read_data(storage_books_name)
     if book_id in data_books:
         del data_books[book_id]
@@ -64,32 +120,67 @@ def delete_book_in_library(book_id: str) -> bool:
 
 
 def find_book_in_library(title: str = None, author: str = None, year: str = None) -> list:
+    """
+    Находит книги в библиотеке по названию, автору и году. Ввод поддерживает
+    регулярные выражения. В случае передачи нескольких аргументов, поиск будет
+    осуществляться по всем аргументам и будут выданы ID всех книг,
+    подходящих по параметрам одного из аргументов.
+
+    Аргументы:
+    title (str, необязательно): Название книги для поиска,
+    author (str, необязательно): Автор книги для поиска,
+    year (str, необязательно): Год книги для поиска/
+
+    Возвращает:
+    list: Список идентификаторов найденных книг. Если не найдено ни одной книги
+    возвращает пустой список. При ошибке в регулярном выражении возвращает пустой
+    список по аргументу с ошибкой, либо найденные книги по остальным аргументам
+    """
     result = []
     data_books = _library_read_data(storage_books_name)
-    try:
-        if title is not None:
-            for book_id, book in data_books.items():
+    if title is not None:
+        for book_id, book in data_books.items():
+            try:
                 if re.fullmatch(title, book[BookBase.title_idx]):
                     result.append(book_id)
-        if author is not None:
-            for book_id, book in data_books.items():
+            except re.error:
+                break
+    if author is not None:
+        for book_id, book in data_books.items():
+            try:
                 if re.fullmatch(author, book[BookBase.author_idx]):
                     result.append(book_id)
-        if year is not None:
-            for book_id, book in data_books.items():
+            except re.error:
+                break
+    if year is not None:
+        for book_id, book in data_books.items():
+            try:
                 if re.fullmatch(str(year), str(book[BookBase.year_idx])):
                     result.append(book_id)
+            except re.error:
+                break
 
-        return result
-    except re.error:
-        return list()
+    return result
 
 
 def get_all_books_in_library() -> dict:
+    """
+    Возвращает все книги в библиотеке в виде словаря
+    """
     return _library_read_data(storage_books_name)
 
 
 def change_book_status(book_id: str, status: bool) -> bool:
+    """
+    Изменяет статус книги в библиотеке.
+
+    Аргументы:
+    book_id (str): Идентификатор книги, статус которой нужно изменить,
+    status (bool): Новый статус книги.
+
+    Возвращает:
+    bool: True, если статус был изменен, False в противном случае.
+    """
     data_books = _library_read_data(storage_books_name)
     if book_id in data_books:
         data_books[book_id][BookBase.status_idx] = status
@@ -100,6 +191,18 @@ def change_book_status(book_id: str, status: bool) -> bool:
 
 
 def get_book_from_id(book_id: str) -> BookBase:
+    """
+    Получает книгу из библиотеки на основе ее идентификатора.
+
+    Аргументы:
+    book_id (str): идентификатор книги, которую нужно получить.
+
+    Возвращает:
+    BookBase: книга с указанным идентификатором.
+
+    Вызывает:
+    Исключение KeyError: если книга с указанным идентификатором не найдена.
+    """
     data_books = _library_read_data(storage_books_name)
     if book_id in data_books:
         book_data = data_books[book_id]
--- Library_management_system/command_line_sub_menu.py
+++ Library_management_system/command_line_sub_menu.py
@@ -1,18 +1,33 @@
+"""
+Этот модуль представляет собой набор подменю, которые выбираются в зависимости от
+нужных пользователю задач. Подменю поддерживать ввод пользователем данных, через
+стандартный ввод и вывод результата. В результате работы подменю производиться и
+выполнение необходимых действий с данными в библиотеке.
+"""
+
 import books_data_library_control as bc
 from custom_string import CustomString
 
 
 class InputInfoBook:
+    """
+     Этот класс отвечает за ввод информации о книге от пользователя.
+     Ввод и вывод через стандартные input и print.
+    """
+
     def __init__(self):
+        # допустимые значения для ввода
         self.title_min_max_length = 1, 200
         self.autor_min_max_length = 1, 100
         self.year_min_max = -7000, 3000
         self.acceptable_status_book = 'i', 'o'
+        # сообщения об ошибках которые могут выводиться пользователю
         self.invalid_message = "Не понял ввод!"
         self.invalid_id_message = "Неверный идентификатор! ID должен содержать две буквы, дефис и число"
+
         self.cs = CustomString()
 
-    def get_title(self):
+    def get_title(self) -> str:
         title = str(input("Введите название книги: "))
         while not self.title_min_max_length[0] <= len(title) <= self.title_min_max_length[1]:
             print(f"{self.invalid_message}\n"
@@ -21,7 +36,7 @@ class InputInfoBook:
             title = str(input("Название книги: "))
         return title
 
-    def get_author(self):
+    def get_author(self) -> str:
         autor = str(input("Введите автора: "))
         while not self.autor_min_max_length[0] <= len(autor) <= self.autor_min_max_length[1]:
             print(f"{self.invalid_message}\n"
@@ -30,7 +45,7 @@ class InputInfoBook:
             autor = str(input("Автор: "))
         return autor
 
-    def get_year(self):
+    def get_year(self) -> int:
         year = input("Введите год издания: ")
         while not (year.lstrip('-').isdigit() and self.year_min_max[0] <= int(year) <= self.year_min_max[1]):
             print(f"{self.invalid_message}\n"
@@ -38,7 +53,7 @@ class InputInfoBook:
             year = input("Год издания: ")
         return int(year)
 
-    def get_status(self):
+    def get_status(self) -> bool:
         status = input(f"Введите статус книги '{self.acceptable_status_book[0]}' (в наличии) или "
                        f"'{self.acceptable_status_book[1]}' (выдана): ")
         while status not in self.acceptable_status_book:
@@ -47,7 +62,7 @@ class InputInfoBook:
                            f"'{self.acceptable_status_book[1]}' (выдана): ")
         return True if status == self.acceptable_status_book[0] else False
 
-    def get_id(self):
+    def get_id(self) -> str:
         id_book = input("Введите ID книги: ")
         while len(id_book) < 4 or not (id_book[:2].isalpha() and id_book[2] == '-' and id_book[3:].isdigit()):
             print(self.cs.warning(self.invalid_id_message))
@@ -56,15 +71,29 @@ class InputInfoBook:
 
 
 class SubMenu(InputInfoBook):
+    """
+    Этот класс отвечает за обработку подменю и работу с книгами
+    и их данными в библиотеке через импортированный модуль
+
+    Методы:
+        add_book(): добавляет новую книгу в библиотеку
+        delete_book(): удаляет книгу из библиотеки
+        find_book(): находит книгу в библиотеке
+        print_all_books(): печатает все книги в библиотеке
+        print_books_from_id(id_book_list: list): печатает книги из указанного списка идентификаторов
+        change_status(): изменяет статус книги (в наличие или выдана)
+    """
+
     def __init__(self):
         super().__init__()
 
-    def add_book(self):
+    def add_book(self) -> None:
         book = bc.BookBase(self.get_title(), self.get_author(), self.get_year())
         id_new_book = bc.add_book_in_library(book)
         print(self.cs.good(f"Книга успешно добавлена, ID: {id_new_book}"))
 
-    def delete_book(self):
+    def delete_book(self) -> bool:
+        """Возвращает True, если удаление прошло успешно, иначе False"""
         id_book = self.get_id()
         if bc.delete_book_in_library(id_book):
             print(self.cs.good(f"Книга с ID: {id_book} успешно удалена"))
@@ -73,7 +102,8 @@ class SubMenu(InputInfoBook):
             print(self.cs.warning(f"Книга с ID: {id_book} не удалена (возможно, такой книги не существует)"))
             return False
 
-    def find_book(self):
+    def find_book(self) -> list:
+        """Возвращает список ID найденных книг"""
         valid_selection_symbol = ('t', 'a', 'y')
         invalid_message = "Не понял выбор!"
         while True:
@@ -102,34 +132,41 @@ class SubMenu(InputInfoBook):
 
         self.print_books_from_id(id_list)
         print(self.cs.info_result(f"Всего найдено книг: {len(id_list)}"), '\n')
+        return id_list
 
-    def print_all_books(self):
+    def print_all_books(self) -> None:
         books_dict = bc.get_all_books_in_library()
         for id_book, book in books_dict.items():
-            print(self.cs.separate('*' * 20) + '\n'
-                  f"ID книги:\t" + self.cs.id_book(f'{id_book}') + '\n'
-                  f"Название:\t{book[bc.BookBase.title_idx]}\n"
-                  f"Автор:\t\t{book[bc.BookBase.author_idx]}\n"
+            print(self.cs.separate('*' * 20) + '\n' +
+                  f"ID книги:\t" + self.cs.id_book(f'{id_book}') + '\n' +
+                  f"Название:\t{book[bc.BookBase.title_idx]}\n" +
+                  f"Автор:\t\t{book[bc.BookBase.author_idx]}\n" +
                   f"Год издания:\t{book[bc.BookBase.year_idx]}\n" +
                   self.cs.good('в наличии') if book[bc.BookBase.status_idx] else
                   self.cs.warning('выдана') + '\n')
 
         print(self.cs.info_result(f"Всего книг в библиотеке: {len(books_dict)}"), '\n')
 
-    def print_books_from_id(self, id_book_list: list):
+    def print_books_from_id(self, id_book_list: list) -> None:
+        """
+        Аргументы:
+            id_book_list (list): Список идентификаторов книг для печати
+        """
         books_dict = bc.get_all_books_in_library()
         for id_book, book in books_dict.items():
             for id_ in id_book_list:
                 if id_ == id_book:
-                    print(self.cs.separate('*' * 20) + '\n'
-                          f"ID книги:\t" + self.cs.id_book(f'{id_book}') + '\n'
-                          f"Название:\t{book[bc.BookBase.title_idx]}\n"
-                          f"Автор:\t\t{book[bc.BookBase.author_idx]}\n"
+                    print(self.cs.separate('*' * 20) + '\n' +
+                          f"ID книги:\t" + self.cs.id_book(f'{id_book}') + '\n' +
+                          f"Название:\t{book[bc.BookBase.title_idx]}\n" +
+                          f"Автор:\t\t{book[bc.BookBase.author_idx]}\n" +
                           f"Год издания:\t{book[bc.BookBase.year_idx]}\n" +
                           self.cs.good('в наличии') if book[bc.BookBase.status_idx] else
                           self.cs.warning('выдана') + '\n')
 
-    def change_status(self):
+    def change_status(self) -> bool:
+        """Возвращает True в случае успешного изменения статуса книги,
+            False в противном случае"""
         id_book = self.get_id()
         if id_book in bc.get_all_books_in_library():
             current_status = bc.get_book_from_id(id_book).status
